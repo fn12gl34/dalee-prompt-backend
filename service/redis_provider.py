75b5fa8b8670daab52ddd1f64623adef52d9c5a8
idunno <dmitry.kuzmin@inten.to>
2022-08-03 14:25:04 +0200

    redis connection fix

--- service/redis_provider.py
+++ service/redis_provider.py
@@ -1,3 +1,4 @@
+import os
 import random
 import uuid
 
@@ -7,7 +8,7 @@ from aredis import StrictRedis
 class RedisProvider:
 
     def __init__(self):
-        self._redis_cluster: StrictRedis = StrictRedis(host='redis://:pb2c7fb1a07102688307b10e1eb913a7c1fe603c71bc160564c22df24a077f5e5@ec2-52-48-130-152.eu-west-1.compute.amazonaws.com', port=14159)
+        self._redis_cluster: StrictRedis = StrictRedis(host=os.environ.get('REDIS_URL'), port=14159)
 
     async def get(self):
         r = await self._redis_cluster.keys('*')
