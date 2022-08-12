32514beeded05d7e6eca2000703cc8cb16e04fd1
idunno <dmitry.kuzmin@inten.to>
2022-08-13 01:04:09 +0300

    fuck cors

--- service/base.py
+++ service/base.py
@@ -1,5 +1,4 @@
 from aiohttp.web_app import Application
-import aiohttp_cors
 
 from service.middlewares import authorization_middleware
 from service.views.base import liveness
@@ -13,14 +12,6 @@ from service.views.schemas import SchemasView
 def create_app() -> Application:
     aiohttp_app = Application()
     aiohttp_app[REDIS_PROVIDER]: RedisProvider = init_redis_provider()
-    cors = aiohttp_cors.setup(aiohttp_app, defaults={
-        "*": aiohttp_cors.ResourceOptions(
-            allow_credentials=True,
-            expose_headers="*",
-            allow_headers="*",
-        )
-    })
-    cors.add(aiohttp_app.router.add_route('GET', '/health/liveness', liveness))
     aiohttp_app.on_startup.extend([init_routes, init_middlewares])
     return aiohttp_app
 
@@ -29,6 +20,7 @@ async def init_routes(aiohttp_app: Application) -> None:
     aiohttp_app.router.add_view('/attributes', AttributesView)
     aiohttp_app.router.add_view('/tags', TagsView)
     aiohttp_app.router.add_view('/schema', SchemasView)
+    aiohttp_app.router.add_route('GET', '/health/liveness', liveness)
 
 
 def init_redis_provider():
