59f6101cda9f72aa024b636332a623379ab9d38f
idunno <dmitry.kuzmin@inten.to>
2022-08-13 13:39:17 +0300

    upd

--- service/base.py
+++ service/base.py
@@ -1,5 +1,5 @@
 from aiohttp.web_app import Application
-import aiohttp_cors
+from aiohttp_middlewares import cors_middleware
 
 from service.middlewares import authorization_middleware
 from service.views.base import liveness
@@ -12,31 +12,16 @@ from service.views.schemas import SchemasView
 
 def create_app() -> Application:
     aiohttp_app = Application()
-    cors = aiohttp_cors.setup(aiohttp_app, defaults={
-        "*": aiohttp_cors.ResourceOptions(
-            allow_credentials=True,
-            expose_headers="*",
-            allow_headers="*"
-        )
-    })
-    cors.add(aiohttp_app.router.add_route('GET', '/health/liveness', liveness))
     # aiohttp_app[REDIS_PROVIDER]: RedisProvider = init_redis_provider()
-    aiohttp_app.on_startup.extend([init_middlewares])
+    aiohttp_app.on_startup.extend([init_routes, init_middlewares])
     return aiohttp_app
 
 
 async def init_routes(aiohttp_app: Application) -> None:
-    cors = aiohttp_cors.setup(aiohttp_app, defaults={
-        "*": aiohttp_cors.ResourceOptions(
-            allow_credentials=True,
-            expose_headers="*",
-            allow_headers="*"
-        )
-    })
-    '''aiohttp_app.router.add_view('/attributes', AttributesView)
+    aiohttp_app.router.add_view('/attributes', AttributesView)
     aiohttp_app.router.add_view('/tags', TagsView)
-    aiohttp_app.router.add_view('/schema', SchemasView)'''
-    cors.add(aiohttp_app.router.add_route('GET', '/health/liveness', liveness))
+    aiohttp_app.router.add_view('/schema', SchemasView)
+    aiohttp_app.router.add_route('GET', '/health/liveness', liveness)
 
 
 def init_redis_provider():
@@ -45,3 +30,4 @@ def init_redis_provider():
 
 async def init_middlewares(aiohttp_app: Application) -> None:
     aiohttp_app.middlewares.append(authorization_middleware)
+    aiohttp_app.middlewares.append(cors_middleware(allow_all=True))
