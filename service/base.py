9d1897f58826596768509057b79247321bd760d5
idunno <dmitry.kuzmin@inten.to>
2022-08-13 13:53:54 +0300

    test

--- service/base.py
+++ service/base.py
@@ -1,5 +1,6 @@
 from aiohttp.web_app import Application
 from aiohttp_middlewares import cors_middleware
+from aiohttp_middlewares.cors import DEFAULT_ALLOW_HEADERS
 
 from service.middlewares import authorization_middleware
 from service.views.base import liveness
@@ -12,7 +13,7 @@ from service.views.schemas import SchemasView
 
 def create_app() -> Application:
     aiohttp_app = Application()
-    # aiohttp_app[REDIS_PROVIDER]: RedisProvider = init_redis_provider()
+    aiohttp_app[REDIS_PROVIDER]: RedisProvider = init_redis_provider()
     aiohttp_app.on_startup.extend([init_routes, init_middlewares])
     return aiohttp_app
 
@@ -29,5 +30,5 @@ def init_redis_provider():
 
 
 async def init_middlewares(aiohttp_app: Application) -> None:
-    aiohttp_app.middlewares.append(cors_middleware(allow_all=True))
+    aiohttp_app.middlewares.append(cors_middleware(allow_all=True, allow_headers=DEFAULT_ALLOW_HEADERS + ('apikey')))
     aiohttp_app.middlewares.append(authorization_middleware)
