89e89e68bf811806c38f9aad557b6448ecd15eb9
idunno <dmitry.kuzmin@inten.to>
2022-08-13 14:23:06 +0300

    test

--- service/views/attributes.py
+++ service/views/attributes.py
@@ -5,12 +5,7 @@ from service.constants import *
 class AttributesView(web.View):
 
     async def get(self):
-        request_data = await self.request.json()
-        if 'id' in request_data:
-            redis_key = f'{self.request.app[REDIS_PROVIDER].ATTRIBUTE}/{request_data.get("id")}'
-            result = self.request.app[REDIS_PROVIDER].get(redis_key)
-        else:
-            result = self.request.app[REDIS_PROVIDER].get_all(self.request.app[REDIS_PROVIDER].ATTRIBUTE)
+        result = self.request.app[REDIS_PROVIDER].get_all(self.request.app[REDIS_PROVIDER].ATTRIBUTE)
         return web.json_response(result)
 
     async def post(self):
