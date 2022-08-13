a5b5e063b2a2d1896bb26fc3d8808b21fd3389f4
idunno <dmitry.kuzmin@inten.to>
2022-08-13 13:01:45 +0300

    test

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os
+'''import os
 
 from aiohttp import web
 
@@ -9,4 +9,29 @@ if __name__ == '__main__':
         app = create_app()
         web.run_app(app, port=os.getenv('PORT'))
     except Exception as e:
-        print('Unhandled exception in platform-restrictions-settings-service, error: %s', e)
+        print('Unhandled exception in platform-restrictions-settings-service, error: %s', e)'''
+import os
+
+import aiohttp_cors
+from aiohttp import web
+from aiohttp.web_response import Response
+
+
+async def t(_):
+    return Response(text='hello')
+
+app = web.Application()
+app.router.add_get("/t", t)
+
+cors = aiohttp_cors.setup(app, defaults={
+    "*": aiohttp_cors.ResourceOptions(
+        allow_credentials=True,
+        expose_headers="*",
+        allow_headers="*"
+    )
+})
+
+for route in list(app.router.routes()):
+    cors.add(route)
+
+web.run_app(app, port=os.getenv('PORT'))
