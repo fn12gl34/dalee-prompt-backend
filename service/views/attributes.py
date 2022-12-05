b9836da1e91166bff0c1fdf11eb7e6a798d44610
Dmitry <dmitry.kuzmin@inten.to>
2022-12-05 23:22:45 +0400

    Update attributes.py

--- service/views/attributes.py
+++ service/views/attributes.py
@@ -4,12 +4,7 @@ from service.constants import *
 
 class AttributesView(web.View):
 
-    async def get(self):
-        result = self.request.app[REDIS_PROVIDER].get_all(self.request.app[REDIS_PROVIDER].ATTRIBUTE)
-        return web.json_response(result)
-
     async def post(self):
         request_data = await self.request.json()
-        redis_key = f'{self.request.app[REDIS_PROVIDER].ATTRIBUTE}/{request_data.get("id")}'
-        self.request.app[REDIS_PROVIDER].set(redis_key, request_data)
+        print(request_data)
         return web.json_response(request_data)
